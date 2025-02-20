1d55d369acfc5c00d520cb500792d1c11e880a29
matheusdpo <matheusoliveira1991@hotmail.com>
2025-02-20 15:27:05 -0300

    config github

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Gera uma versão estática do site
+  basePath: '/my-website', // Substitua pelo nome do repositório
+  assetPrefix: '/my-website/', // Substitua pelo nome do repositório
 };
 
 export default nextConfig;
