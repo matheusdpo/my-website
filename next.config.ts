2ffaf4b71be28960cb87eee811ed777618b84adb
matheusdpo <matheusoliveira1991@hotmail.com>
2025-02-20 15:31:27 -0300

    removed git configs

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Gera uma versão estática do site
-  basePath: '/my-website', // Substitua pelo nome do repositório
-  assetPrefix: '/my-website/', // Substitua pelo nome do repositório
+  /* config options here */
 };
 
 export default nextConfig;
